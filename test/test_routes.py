71a3e374542f24b735398f3fb264760c9e78faca
@putrirokh <putri.rokhmayati@gmail.com>
2023-12-22 09:40:17 +0700

    create some pass tests for root

--- test/test_routes.py
+++ test/test_routes.py
@@ -1,22 +1,45 @@
 import json
 
-from .settings import DEFAULT_PRICE
-
 def test_product_detail_api(client):
-    id = 3
+    # Test retrieving details of an existing product
+    id = 1
     response = client.get(f"/api/products/{id}")
     assert response.status_code == 200
     data = json.loads(response.data)
     assert id == data.get('id')
-    assert DEFAULT_PRICE * id
 
+    # Test retrieving details of a non-existent product
+    non_existent_id = 999
+    response = client.get(f"/api/products/{non_existent_id}")
+    assert response.status_code == 404
 
 def test_product_api(client):
+    # Test retrieving the list of products without any filters
     response = client.get("/api/products")
     assert response.status_code == 200
 
+    # Test retrieving the list of products with a search keyword
+    keyword = "example"
+    response = client.get(f"/api/products?q={keyword}")
+    assert response.status_code == 200
 
-def test_get_cart(client):
-    # route 'api/cart/<id>'
-    assert True
+    # Test retrieving the list of products with multiple SKUs
+    sku_list = "sku1,sku2,sku3"
+    response = client.get(f"/api/products?sku={sku_list}")
+    assert response.status_code == 200
 
+def test_cart_api(client):
+    # Test retrieving the list of active carts
+    response = client.get("/api/cart")
+    assert response.status_code == 200
+
+    # Test creating a new cart
+    cart_data = {
+        "coupon_code": "DISCOUNT",
+        "shipping_fee": 10.0,
+        "cart_items": [{"product_id": 1, "qty": 2}]
+    }
+    response = client.post("/api/cart", json=cart_data)
+    assert response.status_code == 200
+
+    
